fc18149436cba7fd622504a5c2eff69fdb9082b1
Michelangelo <dragonbe+github@gmail.com>
2013-01-04 23:10:21 +0100

    Adding more tests for the auth service

--- tests/application/modules/account/services/AuthTest.php
+++ tests/application/modules/account/services/AuthTest.php
@@ -38,4 +38,34 @@ class Account_Service_AuthTest extends DatabaseTestCase
         $this->assertDataSetsEqual($this->createFlatXMLDataSet(
             TEST_PATH . '/_files/verifyTokenDataset.xml'), $ds);
     }
+    
+    public function testVerifyTokenExists()
+    {
+        $token = 'e896b31e7ea87a0cf38d329e444afe0ff1af7ca7';
+        $service = new Account_Service_Auth();
+        $this->assertTrue($service->tokenExists($token));
+        $this->assertFalse($service->tokenExists('blabla'));
+    }
+    
+    public function testVerifyAccountByEmail()
+    {
+        $email = 'john.doe@example.com';
+        $service = new Account_Service_Auth();
+        $actual = $service->findAccountByEmail($email);
+        
+        $expected = array (
+            'accountId' => 1,
+            'firstName' => 'John',
+            'lastName' => 'Doe',
+            'email' => 'john.doe@example.com',
+            'password' => 'YoOX7hibF4h5.',
+            'token' => '',
+            'created' => '1999-12-31 23:59:59',
+            'modified' => '2000-01-01 00:00:00',
+            'active' => 1,
+        );
+        
+        $this->assertSame($expected, $actual->toArray());
+    }
+    
 }
