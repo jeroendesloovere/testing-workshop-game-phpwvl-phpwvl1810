79aa355cd35a6bd6457939c8b2616c3d0e3c74ca
Michelangelo van Dam <dragonbe+github@gmail.com>
2017-03-19 21:39:14 +0100

    Removing commented code
    This code is no longer necessary to set up database configurations

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -29,25 +29,5 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         return $view;
     }
 
-    /*protected function _initDatabase()
-    {
-        if ($this->hasPluginResource('db')) {
-            $dbResource = $this->getPluginResource('db');
-            if ($dbResource instanceof Zend_Application_Resource_Db) {
-                $dbResource
-                    ->setAdapter('PDO_MySQL')
-                    ->setParams(array (
-                        'host' => getenv('MYSQL_HOSTNAME') ?: null,
-                        'port' => getenv('MYSQL_PORT') ?: null,
-                        'username' => getenv('MYSQL_USERNAME') ?: null,
-                        'password' => getenv('MYSQL_PASSWORD') ?: null,
-                        'dbname' => getenv('MYSQL_DBNAME') ?: null,
-                    ));
-                $dbResource->setIsDefaultTableAdapter(true);
-            }
-        }
-        return $dbResource;
-    }*/
-
 }
 
