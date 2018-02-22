29c71b93e98c0566ced5a02c633e93be261680c6
Michelangelo van Dam <michelangelo@in2it.be>
2018-02-22 21:57:42 +0100

    Moving secrets out of codebase
    Provisioning through the Azure app settings

---
+++ application/configs/env.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * Database settings
+ */
+defined('DB_HOST') ||
+    define('DB_HOST', getenv('DB_HOST'));
+defined('DB_USER') ||
+    define('DB_USER', getenv('DB_HOST'));
+defined('DB_PASS') ||
+    define('DB_PASS', getenv('DB_PASS'));
+defined('DB_NAME') ||
+    define('DB_NAME', getenv('DB_NAME'));
+
+/*
+ * SMTP settings
+ */
+defined('MAIL_TYPE') ||
+    define('MAIL_TYPE', getenv('MAIL_TYPE') ? getenv('MAIL_TYPE') : 'smtp');
+defined('MAIL_HOST') ||
+    define('MAIL_HOST', getenv('MAIL_HOST') ? getenv('MAIL_HOST') : 'localhost');
+defined('MAIL_SSL') ||
+    define('MAIL_SSL', getenv('MAIL_SSL') ? getenv('MAIL_SSL') : 'tls');
+defined('MAIL_PORT') ||
+    define('MAIL_PORT', getenv('MAIL_PORT') ? getenv('MAIL_PORT') : 587);
+defined('MAIL_AUTH') ||
+    define('MAIL_AUTH', getenv('MAIL_AUTH') ? getenv('MAIL_AUTH') : 'login');
+defined('MAIL_USER') ||
+    define('MAIL_USER', getenv('MAIL_USER') ? getenv('MAIL_USER') : '');
+defined('MAIL_PASS') ||
+    define('MAIL_PASS', getenv('MAIL_PASS') ? getenv('MAIL_PASS') : '');
